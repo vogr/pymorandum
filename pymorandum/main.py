91b1a9c353ae65e23bb7239f5777cc62873ad21a
vogier <contact@vogier.fr>
2017-08-19 10:49:56 +0200

    Remove usage of parallel (messes with spaces in filenames), document main()

--- pymorandum/main.py
+++ pymorandum/main.py
@@ -6,11 +6,6 @@ import re
 import shutil
 import subprocess
 
-"""
-import gi
-gi.require_version('Vips', '8.0')
-from gi.repository import Vips
-"""
 import jinja2
 from tqdm import tqdm
 
@@ -26,6 +21,8 @@ def parse_args():
     parser.add_argument('indir_path')
     parser.add_argument('-i', dest='icc_profile_path',
                         default='/usr/share/color/icc/colord/sRGB.icc')
+    parser.add_argument('-c', dest='config_dir_path',
+                        default='pymorandum_config')
     return parser.parse_args()
 
 def hash_bytestr_iter(bytesiter, hasher):
@@ -44,7 +41,8 @@ def gen_hash(f):
     return hash_bytestr_iter(file_as_blockiter(f.open('rb')), hashlib.blake2b(digest_size=32))
 
 
-def create_thumbnails(pictures_to_process, dest, icc_profile=None, delete_icc=False, linear=False):
+def create_thumbnails(pictures_to_process, dest,
+                      icc_profile=None, delete_icc=False, linear=False):
     log.info("Creating thumbnails, destination folder is {}".format(dest))
     dest.mkdir(exist_ok=True)
     for p in pictures_to_process:
@@ -61,18 +59,66 @@ def create_thumbnails(pictures_to_process, dest, icc_profile=None, delete_icc=Fa
     if linear:
         options += ['--linear']
     for size in sizes:
-        incantation = ['parallel',
-                       'vipsthumbnail',
+        incantation = ['vipsthumbnail',
+                       '--vips-progress',
                        '--size', 'x{}'.format(size),
-                       '-o', dest / Path('%s/{}px.jpg\[optimize_coding,strip\]'.format(size))
+                       '-o', dest / Path('%s/{}px.jpg[optimize_coding,strip]'.format(size))
                        #'-o', str(dest / Path('%s/{}px.jpg'.format(size)))
-        ] + options + [':::'] +  [str(p) for p in pictures_to_process]
+        ] + options +  pictures_to_process
+#        incantation = ['parallel',
+#                       'vipsthumbnail',
+#                       '--vips-progress',
+#                       '--size', 'x{}'.format(size),
+#                       '-o', dest / Path('%s/{}px.jpg\[optimize_coding,strip\]'.format(size))
+#                       #'-o', str(dest / Path('%s/{}px.jpg'.format(size)))
+#        ] + options + [':::'] +  [str(p) for p in pictures_to_process]
 
         #log.info(str(incantation))
         subprocess.run(incantation)
 
 
 def main():
+    """
+    Convert a directory of pictures organised in subdirectories
+    into a photo gallery website.
+    The directory structure is the following:
+
+    input_directory
+    ├── Holidays in Rome
+    │   ├── IMG-3.jpg
+    │   ├── IMG-4.jpg
+    │   └── IMG-5.jpg
+    │       ...
+    └── Holidays in Tokyo
+        ├── IMG-52.jpg
+        ├── IMG-53.jpg
+        └── IMG-54.jpg
+            ...
+
+    _site (output directory)
+    ├── Holidays in Rome
+    │   ├── IMG-3
+    │   │   ├── 1024px.jpg
+    │   │   ├── 1280px.jpg
+    │   │   ├── 1920px.jpg
+    │   │   ├── 320px.jpg
+    │   │   ├── 640px.jpg
+    │   │   └── original
+    │   ├── IMG-4 
+    │        ...
+    └── Holidays in Tokyo
+        ├── IMG-52
+        │   ├── 1024px.jpg
+        │   ├── 1280px.jpg
+        │   ├── 1920px.jpg
+        │   ├── 320px.jpg
+        │   ├── 640px.jpg
+        │   └── original
+        ├── IMG-53
+            ...
+    """
+
+
     args = parse_args()
     log.basicConfig(level=getattr(log, args.logLevel))
 
@@ -80,31 +126,42 @@ def main():
 
     indir = Path(args.indir_path).absolute()
     outdir = Path(args.outdir_path).absolute()
+    config_dir = Path(args.config_dir_path).absolute()
     outdir.mkdir(exist_ok=True)
     log.info('Using input directory: {}'.format(indir))
     log.info('Using output directory: {}'.format(outdir))
+    log.info('Reading config from: {}'.format(config_dir))
     
     assets = outdir / Path('assets')
     icc_profile = Path(args.icc_profile_path).absolute()
 
     to_exclude = set([outdir, assets])
-
+    
+    # Save hashes of pictures already present in outdir to later
+    # detetermine whether a picture's thumbnails should be re-generated.
+    # At the same time, delete any output file whose source file is no longer
+    # present.
     outfile_hashes = {}
+    
+   # Iterate collection of pictures (eg 'Holidays in Rome') 
     for d in (d.absolute() for d in outdir.iterdir() if d.is_dir()):
+        # If the whole collection can't be found in input, delete it. 
         if not (indir / (d.relative_to(outdir))).exists():
             log.info("Directory {} doesn't exist anymore, deleting ".format((indir / d.relative_to(outdir)), d))
             shutil.rmtree(d)
+        # Iterate over subdir containing thumbnails of a single picture (eg 'IMG-4')
         for subdir in (s.absolute() for s in d.iterdir()):
             relative = subdir.relative_to(outdir)
             key = relative
-            if not any((indir / relative).with_suffix(x).exists() for x in photo_exts):
+            # Delete single directory when the corresponding input file can't be found
+            if not any((indir / relative).with_suffix(ex).exists() for ex in photo_exts):
                 log.info("Subdirectory {} doesn't exist anymore, deleting {}".format((indir / relative), subdir))
                 shutil.rmtree(subdir)
             else:
                 outfile_hashes[key] = gen_hash(subdir / Path('original'))
 
     photo_dirs = [x for x in indir.iterdir() if (x.is_dir() and x != outdir)]
-    log.info("Exposing photos from the following directories: {}".format(photo_dirs))
+    log.info("Using photos from the following directories: {}".format(photo_dirs))
 
     for d in (d.absolute() for d in photo_dirs):
         pictures_to_process = []
@@ -127,4 +184,5 @@ def main():
                             linear=False)
         else:
             log.info("Nothing to do for directory {}".format(d))
+
 
