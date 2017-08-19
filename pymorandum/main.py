76ae09eae11fb1aae205585c68002de9d8cef1d4
vogier <contact@vogier.fr>
2017-08-19 16:13:29 +0200

    Use ninja to generate thumbnails very quickly

--- pymorandum/main.py
+++ pymorandum/main.py
@@ -1,13 +1,10 @@
 import argparse
 import logging as log
-import hashlib
 from pathlib import Path
-import re
-import shutil
-import subprocess
 
-import jinja2
-from tqdm import tqdm
+import ninja
+#import jinja2
+#from tqdm import tqdm
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -24,58 +21,6 @@ def parse_args():
     parser.add_argument('-c', dest='config_dir_path',
                         default='pymorandum_config')
     return parser.parse_args()
-
-def hash_bytestr_iter(bytesiter, hasher):
-    for block in bytesiter:
-        hasher.update(block)
-    return hasher.hexdigest()
-
-def file_as_blockiter(afile, blocksize=65536):
-    with afile:
-        block = afile.read(blocksize)
-        while len(block) > 0:
-            yield block
-            block = afile.read(blocksize)
-
-def gen_hash(f):
-    return hash_bytestr_iter(file_as_blockiter(f.open('rb')), hashlib.blake2b(digest_size=32))
-
-
-def create_thumbnails(pictures_to_process, dest,
-                      icc_profile=None, delete_icc=False, linear=False):
-    log.info("Creating thumbnails, destination folder is {}".format(dest))
-    dest.mkdir(exist_ok=True)
-    for p in pictures_to_process:
-        s = dest / p.stem
-        s.mkdir()
-        shutil.copy(p, s / Path('original'))
-
-    sizes = [1920, 1280, 1024, 640, 320]
-    options = []
-    if icc_profile is not None and icc_profile.exists():
-        options += ['--eprofile', icc_profile.absolute()]
-    if delete_icc:
-        options += ['--delete']
-    if linear:
-        options += ['--linear']
-    for size in sizes:
-        incantation = ['vipsthumbnail',
-                       '--vips-progress',
-                       '--size', 'x{}'.format(size),
-                       '-o', dest / Path('%s/{}px.jpg[optimize_coding,strip]'.format(size))
-                       #'-o', str(dest / Path('%s/{}px.jpg'.format(size)))
-        ] + options +  pictures_to_process
-#        incantation = ['parallel',
-#                       'vipsthumbnail',
-#                       '--vips-progress',
-#                       '--size', 'x{}'.format(size),
-#                       '-o', dest / Path('%s/{}px.jpg\[optimize_coding,strip\]'.format(size))
-#                       #'-o', str(dest / Path('%s/{}px.jpg'.format(size)))
-#        ] + options + [':::'] +  [str(p) for p in pictures_to_process]
-
-        #log.info(str(incantation))
-        subprocess.run(incantation)
-
 
 def main():
     """
@@ -118,71 +63,88 @@ def main():
             ...
     """
 
-
     args = parse_args()
     log.basicConfig(level=getattr(log, args.logLevel))
 
-    photo_exts = set(['.jpg', '.jpeg', '.png'])
+    config = {}
 
-    indir = Path(args.indir_path).absolute()
-    outdir = Path(args.outdir_path).absolute()
-    config_dir = Path(args.config_dir_path).absolute()
-    outdir.mkdir(exist_ok=True)
-    log.info('Using input directory: {}'.format(indir))
-    log.info('Using output directory: {}'.format(outdir))
-    log.info('Reading config from: {}'.format(config_dir))
+
+    config['indir'] = Path(args.indir_path).absolute()
+    config['outdir'] = Path(args.outdir_path).absolute()
+    config['config_dir'] = Path(args.config_dir_path).absolute()
+    log.info('Using input directory: {}'.format(config['indir']))
+    if not config['indir'].exists():
+        raise Exception("Directory {} doesn't exist".format(config['indir']))
+
+    log.info('Using output directory: {}'.format(config['outdir']))
+    log.info('Reading config from: {}'.format(config['config_dir']))
     
-    assets = outdir / Path('assets')
-    icc_profile = Path(args.icc_profile_path).absolute()
+    config['ninjafile'] = (config['config_dir'] / Path('build.ninja')).absolute()
+    config['assets'] = config['outdir'] / Path('assets')
+    config['icc_profile'] = Path(args.icc_profile_path).absolute()
 
-    to_exclude = set([outdir, assets])
+    config['photo_exts'] = set(['.jpg', '.jpeg', '.png'])
+    config['sizes'] = ['1920', '1280', '1024', '640', '320']
+    config['video_exts'] = set(['.mov', '.avi'])
+    config['codecs'] = ['webm', 'mp4']
+    #mp4: h264, aac
+    #webm: vp8, vorbis
+    config['vips_options'] = []
+    if config['icc_profile'] is not None and config['icc_profile'].exists():
+        config['vips_options'].append('--eprofile {}'.format(config['icc_profile']))
+        config['vips_options'].append('--delete')
+    #config['vips_options'].append('--linear')
+
+    n = ninja.Writer(config['ninjafile'].open('w', encoding='utf-8'))
+    n.variable('builddir', str(config['outdir']))
+    n.variable('vips_options', ' '.join(config['vips_options']))
+    n.variable('ffmpeg_options', '-threads 0')
     
-    # Save hashes of pictures already present in outdir to later
-    # detetermine whether a picture's thumbnails should be re-generated.
-    # At the same time, delete any output file whose source file is no longer
-    # present.
-    outfile_hashes = {}
+    n.rule(name='clean', command='rm -rf {}'.format(config['outdir']))
+    n.rule(name='copy', command='cp $in $out')
+    n.rule(name='rsync', command='rsync -aPzhu $in/ $out')
+    n.rule(name='make_thumbnails',
+           command='vipsthumbnail \
+                    --vips-progress \
+                    --size x$size \
+                    $vips_options \
+                    -o $out[optimize_coding,strip] \
+                    $in'
+           )
+    n.rule(name='ffmpeg-webm',
+           command='ffmpeg i-i $in \
+                    -c:v libvpx -b:v 1M -crf 30 \
+                    -c:a libvorbis -q:a 4 \
+                    $out'
+           )
+    n.rule(name='ffmpeg-mp4',
+           command='ffmpeg -i $in \
+                    -c:v libx264 -crf 20 -preset:v veryslow \
+                    -c:a libfdk_aac -vbr 4 \
+                    -movflags +faststart \
+                    $ffmpeg_options \
+                    $out'
+          )
     
-   # Iterate collection of pictures (eg 'Holidays in Rome') 
-    for d in (d.absolute() for d in outdir.iterdir() if d.is_dir()):
-        # If the whole collection can't be found in input, delete it. 
-        if not (indir / (d.relative_to(outdir))).exists():
-            log.info("Directory {} doesn't exist anymore, deleting ".format((indir / d.relative_to(outdir)), d))
-            shutil.rmtree(d)
-        # Iterate over subdir containing thumbnails of a single picture (eg 'IMG-4')
-        for subdir in (s.absolute() for s in d.iterdir()):
-            relative = subdir.relative_to(outdir)
-            key = relative
-            # Delete single directory when the corresponding input file can't be found
-            if not any((indir / relative).with_suffix(ex).exists() for ex in photo_exts):
-                log.info("Subdirectory {} doesn't exist anymore, deleting {}".format((indir / relative), subdir))
-                shutil.rmtree(subdir)
-            else:
-                outfile_hashes[key] = gen_hash(subdir / Path('original'))
+    n.build(str(config['assets']), 'rsync', inputs=str(config['config_dir'] / Path('assets')))
+    for collection in (d.absolute() for d in config['indir'].iterdir() if d.is_dir()):
+        for f in (p.absolute() for p in collection.iterdir() if
+                        p.is_file() and p.suffix):
+            relative = f.relative_to(config['indir'])
+            original = config['outdir'] / relative / Path('original')
+            n.build(str(original), 'copy', inputs=str(f))
 
-    photo_dirs = [x for x in indir.iterdir() if (x.is_dir() and x != outdir)]
-    log.info("Using photos from the following directories: {}".format(photo_dirs))
+            if f.suffix in config['photo_exts']:
+                for size in config['sizes']:
+                    out = config['outdir'] / relative / Path("{}px.jpg".format(size))
+                    n.build(str(out), 'make_thumbnails',
+                            inputs=str(f), variables={'size':size})
 
-    for d in (d.absolute() for d in photo_dirs):
-        pictures_to_process = []
-        for picture in (p.absolute() for p in d.iterdir() if
-                        p.is_file() and p.suffix in photo_exts):
-            relative = picture.relative_to(indir)
-            key = relative.with_suffix('')
-            h = outfile_hashes.get(key)
-            if h is not None:
-                if h != gen_hash(picture):
-                    log.warn("File has changed: {}".format(picture))
-                    log.warn("Removing {}".format(outdir / key))
-                    shutil.rmtree(outdir / key)
-                    pictures_to_process.append(picture)
-            else:
-                pictures_to_process.append(picture)
-        if pictures_to_process:
-            create_thumbnails(pictures_to_process, outdir / d.relative_to(indir),
-                            icc_profile = icc_profile, delete_icc = True,
-                            linear=False)
-        else:
-            log.info("Nothing to do for directory {}".format(d))
+            elif f.suffix in config['video_exts']:
+                for codec in config['codecs']:
+                    out = config['outdir'] / relative / Path('video.{}'.format(codec))
+                    n.build(str(out), 'ffmpeg-{}'.format(codec), inputs=str(f))
+    ninja.ninja(config['ninjafile'])
 
-
+if __name__ == '__main__':
+    main()
